ba2f80b2217ba274f710e704829b9b0ba2941079
Aleexmarcelo <jojoanete@gmail.com>
2023-05-18 13:10:02 -0300

    Alguns testes e sistema de vida

--- Assets/Script/UI/HealthSystem.cs
+++ Assets/Script/UI/HealthSystem.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
 {
+    public Text txtHealth;
+    private int maxHealth = 20;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
+            maxHealth--;
+            txtHealth.text = maxHealth.ToString();
             Destroy(other.gameObject);
+
+            if (maxHealth <= 0)
+            {
+                SceneManager.LoadScene("GameOver");
+            }
         }
     }
 }
